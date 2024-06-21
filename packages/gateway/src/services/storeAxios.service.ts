ebc7f432ff0886995ca44599e697aaf23714663e
hung.nguyen <hung.nguyen@nexpando.com>
2024-06-21 13:46:38 +0700

    deploy

--- packages/gateway/src/services/storeAxios.service.ts
+++ packages/gateway/src/services/storeAxios.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:7070/',
+  baseURL: 'https://it4995-bend-1.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
